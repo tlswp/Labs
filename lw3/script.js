11ac3d63a53d64de3a07fbba86e1d4d0abb08e5d
OhMyTlswp <54995327+OhMyTlswp@users.noreply.github.com>
2019-09-27 01:02:10 +0300

    fix

--- lw3/script.js
+++ lw3/script.js
@@ -1,24 +1,24 @@
-var myCheck = document.getElementById("theme")
-myMain = document.querySelectorAll("body.main");
-myFalse = document.querySelectorAll("div.weekend .day.false");
-myWeekend = document.querySelectorAll("div.weekend .day");
+var myCheck = document.getElementById('theme')
+myMain = document.querySelectorAll('body.main');
+myFalse = document.querySelectorAll('div.weekend .day.false');
+myWeekend = document.querySelectorAll('div.weekend .day');
 function theme() {
   if (myCheck.checked) {
-    myMain[0].className = "main night";
+    myMain[0].className = 'main night';
     for (let i = 0; i < myWeekend.length; i++) {
-      myWeekend[i].className = "day nigth";
+      myWeekend[i].className = 'day nigth';
     }
     for (let i = 0; i < myFalse.length; i++) {
-      myFalse[i].className = "day false nigth";
+      myFalse[i].className = 'day false nigth';
     }
   }
   else {
-    myMain[0].className = "main";
+    myMain[0].className = 'main';
     for (let i = 0; i < myWeekend.length; i++) {
-      myWeekend[i].className = "day";
+      myWeekend[i].className = 'day';
     }
     for (let i = 0; i < myFalse.length; i++) {
-      myFalse[i].className = "day false";
+      myFalse[i].className = 'day false';
     }
   }
 }
