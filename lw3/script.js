e95908625d49a2c5e6ac4accf318ab8122a88a0f
OhMyTlswp <54995327+OhMyTlswp@users.noreply.github.com>
2019-09-28 09:38:01 +0300

    Checkbox update

--- lw3/script.js
+++ lw3/script.js
@@ -1,9 +1,12 @@
-var myCheck = document.getElementById('theme')
-myMain = document.querySelectorAll('body.main');
-myFalse = document.querySelectorAll('div.weekend .day.false');
+var myCheck = document.getElementById('theme'),
+myMain = document.querySelectorAll('body.main'),
+myFalse = document.querySelectorAll('div.weekend .day.false'),
 myWeekend = document.querySelectorAll('div.weekend .day');
+var checkImg = document.querySelectorAll('img.check');
 function theme() {
   if (myCheck.checked) {
+    checkImg[0].className = 'check none';
+    checkImg[1].className = 'check';
     myMain[0].className = 'main night';
     for (let i = 0; i < myWeekend.length; i++) {
       myWeekend[i].className = 'day nigth';
@@ -14,6 +17,8 @@ function theme() {
   }
   else {
     myMain[0].className = 'main';
+    checkImg[0].className = 'check';
+    checkImg[1].className = 'check none';
     for (let i = 0; i < myWeekend.length; i++) {
       myWeekend[i].className = 'day';
     }
