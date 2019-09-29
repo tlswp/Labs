afdde344d9f7c431f63b17d0fefdc938279eb946
OhMyTlswp <54995327+OhMyTlswp@users.noreply.github.com>
2019-09-29 23:15:58 +0300

    fix

--- lw3/script.js
+++ lw3/script.js
@@ -1,29 +1,15 @@
 var myCheck = document.getElementById('theme'),
-myMain = document.querySelectorAll('body.main'),
-myFalse = document.querySelectorAll('div.weekend .day.false'),
-myWeekend = document.querySelectorAll('div.weekend .day');
+myMain = document.getElementById('calendar-id');
 var checkImg = document.querySelectorAll('img.check');
 function theme() {
   if (myCheck.checked) {
+    myMain.classList.toggle('night');
     checkImg[0].className = 'check none';
     checkImg[1].className = 'check';
-    myMain[0].className = 'main night';
-    for (let i = 0; i < myWeekend.length; i++) {
-      myWeekend[i].className = 'day nigth';
-    }
-    for (let i = 0; i < myFalse.length; i++) {
-      myFalse[i].className = 'day false nigth';
-    }
   }
   else {
-    myMain[0].className = 'main';
+    myMain.classList.toggle('night');
     checkImg[0].className = 'check';
     checkImg[1].className = 'check none';
-    for (let i = 0; i < myWeekend.length; i++) {
-      myWeekend[i].className = 'day';
-    }
-    for (let i = 0; i < myFalse.length; i++) {
-      myFalse[i].className = 'day false';
-    }
   }
 }
