1f447cebd876614f83a5cdacb8754e14e1b3f19f
OhMyTlswp <54995327+OhMyTlswp@users.noreply.github.com>
2019-10-03 22:33:19 +0300

    fix

--- lw3/script.js
+++ lw3/script.js
@@ -1,11 +1,23 @@
 var myCheck = document.getElementById('theme'),
 myMain = document.getElementById('calendar-id'),
+day = document.getElementsByClassName('day'),
 checkImg = document.querySelectorAll('img.check');
+var i = 0;
+function load () {
+   setTimeout(function () {
+      day[i].classList.add('height');
+      i++;
+      if (i < day.length) {
+        load();
+      }
+   }, 10)
+}
+load();   
 function theme() {
   if (myCheck.checked) {
     myMain.classList.toggle('night');
     checkImg[0].className = 'check none';
-    checkImg[1].className = 'check';
+    checkImg[1].className = 'check'; 
   }
   else {
     myMain.classList.toggle('night');
