9a0a3b7e7b8600ddf143bf34418e732341c38ae9
OhMyTlswp <54995327+OhMyTlswp@users.noreply.github.com>
2019-10-15 12:02:09 +0300

    update

--- lw4/script.js
+++ lw4/script.js
@@ -1,52 +1,52 @@
-var currentData = 0,
-dataBlock = document.querySelectorAll('.main .data-block'),
-dataR = document.querySelectorAll('.rigth .data'),
-currentDataBlock = 0,
-currentDataR = 0,
-data = document.querySelectorAll('.left .data');
-function goToData(e) {
-  data[currentData].className = 'data';
-  currentData = (e+data.length)%data.length;
-  data[currentData].className = 'data current';
+var selectedItem = 0,
+directory = document.querySelectorAll('.main .directory'),
+itemRigth = document.querySelectorAll('.rigth .item'),
+selectedDirectory = 0,
+selectedItemRigth = 0,
+item = document.querySelectorAll('.left .item');
+function goToItem(elementItem) {
+  item[selectedItem].className = 'item';
+  selectedItem = (elementItem+item.length)%item.length;
+  item[selectedItem].className = 'item selected';
 }
-function goToDataR(a) {
-  dataR[currentDataR].className = 'data';
-  currentDataR = (a+dataR.length)%dataR.length;
-  dataR[currentDataR].className = 'data current';
+function goToItemRigth(elementItemRigth) {
+  itemRigth[selectedItemRigth].className = 'item';
+  selectedItemRigth = (elementItemRigth+itemRigth.length)%itemRigth.length;
+  itemRigth[selectedItemRigth].className = 'item selected';
 }
-function goToDataBlock(b) {
-  dataBlock[currentDataBlock].className = 'data-block';
-  currentDataBlock = (b+dataBlock.length)%dataBlock.length;
-  dataBlock[currentDataBlock].className = 'data-block current';
+function goToDirectory(elementDirectory) {
+  directory[selectedDirectory].className = 'directory';
+  selectedDirectory = (elementDirectory+directory.length)%directory.length;
+  directory[selectedDirectory].className = 'directory selected';
 }
 document.addEventListener('keydown', function(event) {
   if (event.code === 'ArrowRight') {
-    goToDataBlock(currentDataBlock+1);
+    goToDirectory(selectedDirectory+1);
   }
 });
 document.addEventListener('keydown', function(event) {
   if (event.code === 'ArrowLeft') {
-    goToDataBlock(currentDataBlock-1);
+    goToDirectory(selectedDirectory-1);
   }
 });
 document.addEventListener('keydown', function(event) {
-  if (event.code === 'ArrowDown' && currentDataBlock==0) {
-    goToData(currentData+1);
+  if (event.code === 'ArrowDown' && selectedDirectory==0) {
+    goToItem(selectedItem+1);
   }
 });
 document.addEventListener('keydown', function(event) {
-  if (event.code === 'ArrowUp' && currentDataBlock==0) {
-    goToData(currentData-1);
+  if (event.code === 'ArrowUp' && selectedDirectory==0) {
+    goToItem(selectedItem-1);
   }
 });
 
 document.addEventListener('keydown', function(event) {
-  if (event.code === 'ArrowDown' && currentDataBlock==1) {
-    goToDataR(currentDataR+1);
+  if (event.code === 'ArrowDown' && selectedDirectory==1) {
+    goToItemRigth(selectedItemRigth+1);
   }
 });
 document.addEventListener('keydown', function(event) {
-  if (event.code === 'ArrowUp' && currentDataBlock==1) {
-    goToDataR(currentDataR-1);
+  if (event.code === 'ArrowUp' && selectedDirectory==1) {
+    goToItemRigth(selectedItemRigth-1);
   }
 });
