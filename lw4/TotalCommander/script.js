d0a9929a53d9e0087c8fdf81778cfc83f5dc812c
OhMyTlswp <54995327+OhMyTlswp@users.noreply.github.com>
2019-10-17 22:29:58 +0300

    update

--- lw4/TotalCommander/script.js
+++ lw4/TotalCommander/script.js
@@ -1,52 +1,55 @@
 var selectedItem = 0,
-directory = document.querySelectorAll('.main .directory'),
-itemRigth = document.querySelectorAll('.rigth .item'),
-selectedDirectory = 0,
-selectedItemRigth = 0,
-item = document.querySelectorAll('.left .item');
+  directory = document.querySelectorAll('.main .directory'),
+  itemRigth = document.querySelectorAll('.rigth .item'),
+  selectedDirectory = 0,
+  selectedItemRigth = 0,
+  item = document.querySelectorAll('.left .item');
+
 function goToItem(elementItem) {
   item[selectedItem].className = 'item';
-  selectedItem = (elementItem+item.length)%item.length;
+  selectedItem = (elementItem + item.length) % item.length;
   item[selectedItem].className = 'item selected';
 }
+
 function goToItemRigth(elementItemRigth) {
   itemRigth[selectedItemRigth].className = 'item';
-  selectedItemRigth = (elementItemRigth+itemRigth.length)%itemRigth.length;
+  selectedItemRigth = (elementItemRigth + itemRigth.length) % itemRigth.length;
   itemRigth[selectedItemRigth].className = 'item selected';
 }
+
 function goToDirectory(elementDirectory) {
   directory[selectedDirectory].className = 'directory';
-  selectedDirectory = (elementDirectory+directory.length)%directory.length;
+  selectedDirectory = (elementDirectory + directory.length) % directory.length;
   directory[selectedDirectory].className = 'directory selected';
 }
 document.addEventListener('keydown', function(event) {
   if (event.code === 'ArrowRight') {
-    goToDirectory(selectedDirectory+1);
+    goToDirectory(selectedDirectory + 1);
   }
 });
 document.addEventListener('keydown', function(event) {
   if (event.code === 'ArrowLeft') {
-    goToDirectory(selectedDirectory-1);
+    goToDirectory(selectedDirectory - 1);
   }
 });
 document.addEventListener('keydown', function(event) {
-  if (event.code === 'ArrowDown' && selectedDirectory==0) {
-    goToItem(selectedItem+1);
+  if (event.code === 'ArrowDown' && selectedDirectory == 0) {
+    goToItem(selectedItem + 1);
   }
 });
 document.addEventListener('keydown', function(event) {
-  if (event.code === 'ArrowUp' && selectedDirectory==0) {
-    goToItem(selectedItem-1);
+  if (event.code === 'ArrowUp' && selectedDirectory == 0) {
+    goToItem(selectedItem - 1);
   }
 });
 
 document.addEventListener('keydown', function(event) {
-  if (event.code === 'ArrowDown' && selectedDirectory==1) {
-    goToItemRigth(selectedItemRigth+1);
+  if (event.code === 'ArrowDown' && selectedDirectory == 1) {
+    goToItemRigth(selectedItemRigth + 1);
   }
 });
 document.addEventListener('keydown', function(event) {
-  if (event.code === 'ArrowUp' && selectedDirectory==1) {
-    goToItemRigth(selectedItemRigth-1);
+  if (event.code === 'ArrowUp' && selectedDirectory == 1) {
+    goToItemRigth(selectedItemRigth - 1);
   }
 });
