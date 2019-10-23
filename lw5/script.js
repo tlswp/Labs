a08f9d4be40d1f18ff3303bfa1fbfd0e141d2690
OhMyTlswp <54995327+OhMyTlswp@users.noreply.github.com>
2019-10-23 08:51:17 +0300

    upd

---
+++ lw5/script.js
@@ -0,0 +1,17 @@
+var text = "Веб-страница - самостоятельная часть веб-сайта; документ, снабженный уникальным адресом (URL). Веб-страница может иметь статическое или динамическое построение. Обычно веб-страницы организуется в виде гипертекста с включениями текста, графики, звука, видео или анимацию. В сети Интернет просмотр веб-страниц осуществляется посредством браузера.";
+var myText = document.getElementsByClassName("text");
+var searchStr = document.getElementsByClassName('search')
+var newStr = document.getElementsByClassName('new')
+
+function replaceText() {
+  var regex = new RegExp(searchStr[0].value, 'g');
+  let matchAl = text.search(regex);
+  if (matchAl > 0) {
+    var regex = new RegExp(searchStr[0].value, 'g');
+    text = text.replace(regex, newStr[0].value);
+    myText[0].innerHTML = text;
+    matchAl = text.matchAll(regex);
+  } else {
+    alert('Ничего не найдено!!!');
+  }
+}
