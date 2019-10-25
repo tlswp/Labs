11bfb449d71533baebcd984b31310809b34ccfd3
OhMyTlswp <54995327+OhMyTlswp@users.noreply.github.com>
2019-10-25 14:27:31 +0300

    Fix

--- lw5/task1/script.js
+++ lw5/task1/script.js
@@ -1,12 +1,12 @@
-var text = "Веб-страница - самостоятельная часть веб-сайта; документ, снабженный уникальным адресом (URL). Веб-страница может иметь статическое или динамическое построение. Обычно веб-страницы организуется в виде гипертекста с включениями текста, графики, звука, видео или анимацию. В сети Интернет просмотр веб-страниц осуществляется посредством браузера.";
-var myText = document.getElementsByClassName("text");
+var text = 'Веб-страница - самостоятельная часть веб-сайта; документ, снабженный уникальным адресом (URL). Веб-страница может иметь статическое или динамическое построение. Обычно веб-страницы организуется в виде гипертекста с включениями текста, графики, звука, видео или анимацию. В сети Интернет просмотр веб-страниц осуществляется посредством браузера.';
+var myText = document.getElementsByClassName('text');
 var searchStr = document.getElementsByClassName('search')
 var newStr = document.getElementsByClassName('new')
 
 function replaceText() {
   var myRegExp = new RegExp(searchStr[0].value, 'gi');
   var index = text.search(myRegExp);
-  if (index >= 0 && searchStr[0].value != "") {
+  if (index >= 0 && searchStr[0].value != '') {
     text = text.replace(myRegExp, newStr[0].value);
     myText[0].innerHTML = text;
 
