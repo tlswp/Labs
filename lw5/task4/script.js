ab3ae224b9f17a7a5d4e10e99f71ed35bfcf5cd9
OhMyTlswp <54995327+OhMyTlswp@users.noreply.github.com>
2019-11-10 23:06:53 +0300

    upd lw5 added task4

--- lw5/task4/script.js
+++ lw5/task4/script.js
@@ -1,3 +1,22 @@
-var theBigDay = new Date();
-theBigDay.setHours(7, 50);
-alert(theBigDay);
+function isTimeRangesIntersect(timeRange1, timeRange2) {
+  if (Array.isArray(timeRange1) && Array.isArray(timeRange2)) {
+    if (/\d{2}:{1}(?=\d{2}){5}/.test(timeRange1[0]) && /\d{2}:(?=\d{2})/.test(timeRange1[1]) &&
+      /\d{2}:(?=\d{2})/.test(timeRange2[0]) && /\d{2}:(?=\d{2})/.test(timeRange2[1]) &&
+      timeRange1[0].length === 5 && timeRange1[1].length === 5 &&
+      timeRange2[0].length === 5 && timeRange2[1].length === 5) {
+      timeRange1Minutes1 = timeRange1[0];
+      timeRange1Minutes2 = timeRange1[1];
+      timeRange2Minutes1 = timeRange2[0];
+      timeRange2Minutes2 = timeRange2[1];
+      if (timeRange1[1] < '25' && timeRange2[0] < '25' && timeRange1Minutes1[3] < '6' &&
+        timeRange1Mintesus2[3] < '6' && timeRange2Minutes1[3] < '6' && timeRange2Minutes2[3] < '6' &&
+        timeRange1[0] < timeRange1[1] && timeRange2[0] < timeRange2[1]) {
+        if (timeRange1[1] >= timeRange2[0]) {
+          return true;
+        }
+      }
+    }
+  }
+  return false;
+}
+//isTimeRangesIntersect(['08:30', '09:30'], ['10:30', '12:00'])
