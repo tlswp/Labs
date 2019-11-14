234728cb5d7d9b8e8f42a19d8a8f96649c20d902
OhMyTlswp <54995327+OhMyTlswp@users.noreply.github.com>
2019-11-15 01:38:54 +0300

    upd lw6 added task3 and fixed lw5 task4

--- lw5/task4/script.js
+++ lw5/task4/script.js
@@ -1,17 +1,10 @@
 function isTimeRangesIntersect(timeRange1, timeRange2) {
   if (Array.isArray(timeRange1) && Array.isArray(timeRange2)) {
-    if (/\d{2}:{1}(?=\d{2})/.test(timeRange1[0]) && /\d{2}:(?=\d{2})/.test(timeRange1[1]) &&
-      /\d{2}:(?=\d{2})/.test(timeRange2[0]) && /\d{2}:(?=\d{2})/.test(timeRange2[1]) &&
-      timeRange1[0].length === 5 && timeRange1[1].length === 5 &&
-      timeRange2[0].length === 5 && timeRange2[1].length === 5) {
-      var timeRange1Minutes1 = timeRange1[0];
-      var timeRange1Minutes2 = timeRange1[1];
-      var timeRange2Minutes1 = timeRange2[0];
-      var timeRange2Minutes2 = timeRange2[1];
+    if (/\d{2}:{1}(?=[0-5]\d$)/.test(timeRange1[0]) && /\d{2}:(?=[0-5]\d$)/.test(timeRange1[1]) &&
+      /\d{2}:(?=[0-5]\d$)/.test(timeRange2[0]) && /\d{2}:(?=[0-5]\d$)/.test(timeRange2[1])) {
       if (timeRange1[0] < '23' && timeRange1[1] < '23' && timeRange2[0] < '23' && timeRange2[1] < '23' &&
-        timeRange1Minutes1[3] < '6' && timeRange1Minutes2[3] < '6' && timeRange2Minutes1[3] < '6' &&
-        timeRange2Minutes2[3] < '6' && timeRange1[0] < timeRange1[1] && timeRange2[0] < timeRange2[1]) {
-        if (timeRange1[1] >= timeRange2[0]) {
+        timeRange1[0] < timeRange1[1] && timeRange2[0] < timeRange2[1]) {
+        if (timeRange1[1] >= timeRange2[0] && timeRange1[0] <= timeRange2[1]) {
           return true;
         }
       }
---
+++ lw6/task3/script.js
@@ -0,0 +1,42 @@
+var button = document.querySelector('.form-submit');
+window.onsubmit = function() {
+  return false;
+};
+(function validate() {
+  var formItemText = document.querySelectorAll('.form-item__text'),
+    formItemError = document.querySelectorAll('.form-item__error');
+
+  function validateName() {
+    if (/[А-Яа-я]+\s[А-Яа-я]+\s[А-Яа-я]+$/.test(formItemText[0].value)) {
+      formItemError[0].classList.remove('form-item__error_visible');
+    } else {
+      formItemError[0].classList.add('form-item__error_visible');
+    }
+  }
+
+  function validateEmail() {
+    if (/\w+@\w+\.[A-Za-z]*[A-Za-z]\b/.test(formItemText[1].value)) {
+
+      formItemError[1].classList.remove('form-item__error_visible');
+    } else {
+      formItemError[1].classList.add('form-item__error_visible');
+    }
+  }
+
+  function validatePassword() {
+    if (/(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,16}$/.test(formItemText[2].value)) {
+
+      formItemError[2].classList.remove('form-item__error_visible');
+    } else {
+      formItemError[2].classList.add('form-item__error_visible');
+    }
+  }
+  formItemText[0].addEventListener('blur', validateName);
+  formItemText[1].addEventListener('blur', validateEmail);
+  formItemText[2].addEventListener('blur', validatePassword);
+  button.onclick = function() {
+    validateName();
+    validateEmail();
+    validatePassword();
+  };
+})();
