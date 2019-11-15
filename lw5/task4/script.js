fe995fa76a7314916470c8387ca5893dbdf4f1c0
OhMyTlswp <54995327+OhMyTlswp@users.noreply.github.com>
2019-11-15 15:04:50 +0300

    fixed lw5 task4

--- lw5/task4/script.js
+++ lw5/task4/script.js
@@ -1,13 +1,17 @@
 function isTimeRangesIntersect(timeRange1, timeRange2) {
+  var validateTimeRanges = /\d{2}:{1}(?=[0-5]\d$)/.test(timeRange1[0]) && 
+    /\d{2}:(?=[0-5]\d$)/.test(timeRange1[1]) &&
+    /\d{2}:(?=[0-5]\d$)/.test(timeRange2[0]) && 
+    /\d{2}:(?=[0-5]\d$)/.test(timeRange2[1]) && 
+    timeRange1[0] < '23' && timeRange1[1] < '23' && 
+    timeRange2[0] < '23' && timeRange2[1] < '23' &&
+    timeRange1[0] < timeRange1[1] && 
+    timeRange2[0] < timeRange2[1] && 
+    timeRange1[1] >= timeRange2[0] && 
+    timeRange1[0] <= timeRange2[1];
   if (Array.isArray(timeRange1) && Array.isArray(timeRange2)) {
-    if (/\d{2}:{1}(?=[0-5]\d$)/.test(timeRange1[0]) && /\d{2}:(?=[0-5]\d$)/.test(timeRange1[1]) &&
-      /\d{2}:(?=[0-5]\d$)/.test(timeRange2[0]) && /\d{2}:(?=[0-5]\d$)/.test(timeRange2[1])) {
-      if (timeRange1[0] < '23' && timeRange1[1] < '23' && timeRange2[0] < '23' && timeRange2[1] < '23' &&
-        timeRange1[0] < timeRange1[1] && timeRange2[0] < timeRange2[1]) {
-        if (timeRange1[1] >= timeRange2[0] && timeRange1[0] <= timeRange2[1]) {
-          return true;
-        }
-      }
+    if (validateDate) {
+      return true;
     }
   }
   return false;
