3d47be9a7cf1170b1eccb1304b5936586e9b265b
OhMyTlswp <54995327+OhMyTlswp@users.noreply.github.com>
2019-11-20 19:21:30 +0300

    fixed lw6 task2

--- lw6/src/task3/script.js
+++ lw6/src/task3/script.js
@@ -1,14 +1,14 @@
 'use strict';
-var button = document.querySelector('.form-submit');
+var button = document.querySelector('.form_submit');
 window.onsubmit = function() {
   return false;
 };
 (function validate() {
-  var formItemText = document.querySelectorAll('.form-item__text'),
+  var formItemInput = document.querySelectorAll('.form-item__input'),
     formItemError = document.querySelectorAll('.form-item__error');
 
   function validateName() {
-    if (/[А-Яа-я]+\s[А-Яа-я]+\s[А-Яа-я]+$/.test(formItemText[0].value)) {
+    if (/[А-Яа-я]+\s[А-Яа-я]+\s[А-Яа-я]+$/.test(formItemInput[0].value)) {
       formItemError[0].classList.remove('form-item__error_visible');
     } else {
       formItemError[0].classList.add('form-item__error_visible');
@@ -16,7 +16,7 @@ window.onsubmit = function() {
   }
 
   function validateEmail() {
-    if (/\w+@\w+\.[A-Za-z]*[A-Za-z]\b/.test(formItemText[1].value)) {
+    if (/\w+@\w+\.[A-Za-z]*[A-Za-z]\b/.test(formItemInput[1].value)) {
 
       formItemError[1].classList.remove('form-item__error_visible');
     } else {
@@ -25,16 +25,16 @@ window.onsubmit = function() {
   }
 
   function validatePassword() {
-    if (/(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,16}$/.test(formItemText[2].value)) {
+    if (/(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,16}$/.test(formItemInput[2].value)) {
 
       formItemError[2].classList.remove('form-item__error_visible');
     } else {
       formItemError[2].classList.add('form-item__error_visible');
     }
   }
-  formItemText[0].addEventListener('blur', validateName);
-  formItemText[1].addEventListener('blur', validateEmail);
-  formItemText[2].addEventListener('blur', validatePassword);
+  formItemInput[0].addEventListener('blur', validateName);
+  formItemInput[1].addEventListener('blur', validateEmail);
+  formItemInput[2].addEventListener('blur', validatePassword);
   button.onclick = function() {
     validateName();
     validateEmail();
