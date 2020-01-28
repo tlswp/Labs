4dbaa7be9e7d978e2030a19ad6c734898ef6eb68
OhMyTlswp <54995327+OhMyTlswp@users.noreply.github.com>
2020-01-28 23:14:36 +0300

    update

--- lw3/script.js
+++ lw3/script.js
@@ -1,27 +1,34 @@
 var myCheck = document.getElementById('theme'),
-myMain = document.getElementById('main'),
-day = document.getElementsByClassName('day'),
-checkImg = document.querySelectorAll('img.check');
+  myMain = document.getElementById('main'),
+  day = document.getElementsByClassName('day'),
+  checkImg = document.querySelectorAll('img.check');
 var i = 0;
-function load () {
-   setTimeout(function () {
-      day[i].classList.add('height');
-      i++;
-      if (i < day.length) {
-        load();
-      }
-   }, 10)
+
+function load() {
+  setTimeout(function() {
+    day[i].classList.add('height');
+    i++;
+    if (i < day.length) {
+      load();
+    }
+  }, 10)
 }
-load();   
+load();
+
 function theme() {
   if (myCheck.checked) {
     myMain.classList.toggle('night');
     checkImg[0].className = 'check none';
-    checkImg[1].className = 'check'; 
-  }
-  else {
+    checkImg[1].className = 'check';
+  } else {
     myMain.classList.toggle('night');
     checkImg[0].className = 'check';
     checkImg[1].className = 'check none';
   }
 }
+var arr = [];
+while (arr.length < 4) {
+  var r = Math.floor(Math.random() * 3) + 1;
+  if (arr.indexOf(r) === -1) arr.push(r);
+}
+console.log(arr);
